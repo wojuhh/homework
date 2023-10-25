be961bd789b86ad99744a4f6bf74ca66829f19a3
曜灵 <1104606488@qq.com>
2023-10-25 21:19:39 +0800

    注释版

--- 数据结构实验/线性表/2.cpp
+++ 数据结构实验/线性表/2.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 
-typedef struct LNode
+typedef struct LNode    //定义单链表结点类型
 {
     int data;
     struct LNode *next;
-}LNode, *LinkList;
+}LNode, *LinkList;  //LinkList为指向结构体LNode的指针类型
 
 LinkList insertList(LinkList &L, int x)
 {
@@ -16,7 +16,7 @@ LinkList insertList(LinkList &L, int x)
     while(p->next!=NULL)p=p->next;
     p->next=q;
     return L;
-}
+}   //在单链表L的末尾插入元素x
 
 LinkList swapList(LinkList &L, int x){
     LinkList p=L->next;
@@ -33,7 +33,7 @@ LinkList swapList(LinkList &L, int x){
         p->next=q;
     }
     return L;
-}
+}   //将单链表L中所有小于x的元素移到大于或等于x的元素之前
 
 void output(LinkList &L)
 {
@@ -43,7 +43,7 @@ void output(LinkList &L)
         cout<<p->data<<",";
         p=p->next;
     }
-}
+}   //输出单链表L
 
 int main()
 {
