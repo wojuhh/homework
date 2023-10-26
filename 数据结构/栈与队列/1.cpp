fcfc99eb6e977ab0a2844301e7579eb1a8798fe7
曜灵 <1104606488@qq.com>
2023-10-26 22:05:36 +0800

    第一题

--- 数据结构/栈与队列/1.cpp
+++ 数据结构/栈与队列/1.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 #define STACK_INIT_SIZE 100
 #define STACKINCREMENT 10
-typedef char SElemType;
-typedef struct{
+typedef char SElemType;     //定义栈元素类型
+typedef struct{            //定义顺序栈结构
     SElemType *base;
     SElemType *top;
     int stacksize;
-}SqStack;
+}SqStack;                //定义顺序栈类型
 
 bool InitStack(SqStack &S){
     S.base = (SElemType *)malloc(STACK_INIT_SIZE*sizeof(SElemType));
@@ -15,7 +15,7 @@ bool InitStack(SqStack &S){
     S.top = S.base;
     S.stacksize = STACK_INIT_SIZE;
     return true;
-}
+}   //初始化栈
 
 bool Push(SqStack &S, SElemType e){
     if(S.top - S.base >= S.stacksize){
@@ -26,19 +26,19 @@ bool Push(SqStack &S, SElemType e){
     }
     *S.top++ = e;
     return true;
-}
+}   //入栈
 
 bool Pop(SqStack &S, SElemType &e){
     if(S.top == S.base) return false;
     e = *--S.top;
     return true;
-}
+}   //出栈
 
 bool GetTop(SqStack S, SElemType &e){
     if(S.top == S.base) return false;
     e = *(S.top - 1);
     return true;
-}
+}   //取栈顶元素
 
 char Precede(char a, char b){
     switch(a){
@@ -113,42 +113,50 @@ char Precede(char a, char b){
                 case '#': return '=';
             }
     }
-}
+}   //比较运算符优先级
 
 int main(){
-    SqStack OPTR;
-    SqStack OPND;
-    InitStack(OPTR);
-    InitStack(OPND);
-    Push(OPTR, '#');
+    SqStack OPTR;   //定义运算符栈
+    
+    InitStack(OPTR);    //初始化栈
+    
+    Push(OPTR, '#');    //初始化栈顶元素为#
     char c;
-    c==getchar();
-    while(c!='#'||GetTop(OPTR, c)!='#'){
-        if(c>='0'&&c<='9'){
-            Push(OPND, c-'0');
+    c=getchar();
+    while(c!='#'){
+        if(c>='A'&&c<='Z'){
+            cout<<c;
             c=getchar();
-        }
+        }   //如果是操作数则输出
         else{
             char theta;
-            GetTop(OPTR, theta);
-            switch(Precede(theta, c)){
-                case '<':
-                    Push(OPTR, c);
-                    c=getchar();
-                    break;
-                case '=':
-                    Pop(OPTR, theta);
-                    c=getchar();
-                    break;
-                case '>':
-                    Pop(OPTR, theta);
-                    char a, b;
-                    Pop(OPND, b);
-                    Pop(OPND, a);
-                    //Push(OPND, Operate(a, theta, b));
-                    break;
-            }
+            int flag=0;
+            while(1){
+                GetTop(OPTR, theta);
+                switch(Precede(theta, c)){
+                    case '<':
+                        Push(OPTR, c);
+                        c=getchar();
+                        flag=1;
+                        break;
+                    case '=':
+                        Pop(OPTR, theta);
+                        c=getchar();
+                        flag=1;
+                        break;
+                    case '>':
+                        Pop(OPTR, theta);
+                        cout<<theta;
+                        break;
+                }
+                if(flag) break;
+            }       //如果是运算符则与栈顶元素比较优先级，根据优先级进行相应操作
         }
-    }
+    }   //读入字符，判断是运算符还是操作数，是操作数则输出，是运算符则与栈顶元素比较优先级，根据优先级进行相应操作
+    while(OPTR.top!=OPTR.base+1){
+        char theta;
+        Pop(OPTR, theta);
+        cout<<theta;
+    }   //输出栈中剩余运算符
     return 0;   
 }
